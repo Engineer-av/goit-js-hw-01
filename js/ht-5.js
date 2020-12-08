dddd9b152057213cb5301c5b82327c144a2bfab7
Anna Vasyliuk <70869212+Engineer-av@users.noreply.github.com>
2020-12-08 15:10:15 +0100

    delete double code

--- js/ht-5.js
+++ js/ht-5.js
@@ -3,7 +3,7 @@ const countryDelivery =
   message &&
   message.charAt(0).toUpperCase() + message.substring(1).toLowerCase();
 
-let price = 0;
+let price;
 let country;
 let result;
 
@@ -15,35 +15,34 @@ switch (countryDelivery) {
   case "Китай":
     price = 100;
     country = "Китай";
-    result = `Доставка в ${country} будет стоить ${price} кредитов`;
     break;
 
   case "Чили":
     price = 250;
     country = "Чили";
-    result = `Доставка в ${country} будет стоить ${price} кредитов`;
     break;
 
   case "Австралия":
     price = 170;
     country = "Австралию";
-    result = `Доставка в ${country} будет стоить ${price} кредитов`;
     break;
 
   case "Индия":
     price = 80;
     country = "Индию";
-    result = `Доставка в ${country} будет стоить ${price} кредитов`;
     break;
 
   case "Ямайка":
     price = 120;
     country = "Ямайку";
-    result = `Доставка в ${country} будет стоить ${price} кредитов`;
     break;
 
   default:
     result = "В вашей стране доставка не доступна";
 }
 
-alert(result);
+console.log(result);
+
+if (country) {
+  console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
+}
